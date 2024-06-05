c3045e5add7e950cdbfe90e51fe0b83726bb2c6a
nikhila2001 <nsargam58@gmail.com>
2024-06-05 14:06:08 +0530

    edit profile feature

--- frontend/src/Pages/Profile.jsx
+++ frontend/src/Pages/Profile.jsx
@@ -5,12 +5,20 @@ import Logout from "../components/Logout";
 import { fetchUserDetails } from "../utils/userUtils";
 import { Navigate } from "react-router-dom";
 import toast from "react-hot-toast";
+import axios from "axios";
 
-function Profile() {
+const host = "http://localhost:4000/api";
+
+function Profile({ toggleDropdown }) {
   const { isAuth } = useContext(AppContext);
   console.log(isAuth, "isAuth");
   const [userDetails, setUserDetails] = useState({});
   const [editProfile, setEditprofile] = useState(false);
+  const [updatedDetails, setUpdatedDetails] = useState({
+    username: "",
+    address: "",
+    phone: "",
+  });
 
   useEffect(() => {
     const getUserDetails = async () => {
@@ -28,9 +36,34 @@ function Profile() {
 
   const handleEditProfile = () => {
     console.log("edit button clicked");
+    toggleDropdown(true);
     setEditprofile(true);
-    
-  }
+  };
+
+  const handleSaveProfile = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const headers = {
+        "Content-Type": "application/json",
+        token: token,
+      };
+      const response = await axios.post(
+        `${host}/user/editUser`,
+        updatedDetails,
+        { headers }
+      );
+      toast.success(response.data.message);
+
+      // Fetch updated user details after successful update
+      const updatedUserDetails = await fetchUserDetails();
+      setUserDetails(updatedUserDetails); // Update userDetails state
+
+      // Clear the edit mode
+      setEditprofile(false);
+    } catch (error) {
+      toast.error("Failed to update profiel", error.message);
+    }
+  };
 
   if (!isAuth) return <Navigate to="/login" />;
 
@@ -38,41 +71,113 @@ function Profile() {
     <div>
       {isAuth && localStorage.getItem("token") && (
         <>
-        {/*user details  */}
-          <h3 className="text-center">
-          <i className="bi bi-person-circle"></i>
-          </h3>
-          <p className="text-capitalize fw-bold text-center mb-0">{userDetails.username}</p>
-          <p className="text-center"><small className="text-secondary mb-0">{userDetails.email}</small></p>
-          
-          <Logout />
-          <button className="btn text-primary" onClick={handleEditProfile}>edit</button>
+          {/*user details  */}
+          <div className="d-flex justify-content-between">
+            <h3 className="text-start">
+              <i className="bi bi-person-circle"></i>
+            </h3>
+            <Logout />
+          </div>
+
+          <p className="text-capitalize fw-bold text-start mb-0">
+            {userDetails.username}
+          </p>
+          <p className="custom-color text-start mb-0">
+            <small className="mb-0">{userDetails.email}</small>
+          </p>
+          {updatedDetails ? (
+            <>
+              <small className="custom-color text-capitalize mb-0">
+                <i class="bi bi-geo-alt"></i>&nbsp;{`${userDetails.address}`}
+              </small>
+              <small className="custom-color d-block">
+                <i class="bi bi-telephone"></i>&nbsp;{`${userDetails.phone}`}
+              </small>
+            </>
+          ) : ("")}
+
           {/* profile edit form */}
-          {editProfile && (
-          <>
-            <div className="d-flex">
-             {/*Name field*/}
-          <label htmlFor="username">Name</label>
-          <input type="text" id="username" value={userDetails.username}/>
-          </div>
-           {/*Address field*/}
-          <div className="d-flex">
-          <label htmlFor="location">Address</label>
-          <input type="address" id="location" />
-          </div>
-             {/*phone field*/}
-          <div className="d-flex">
-          <label htmlFor="phone">Phone</label>
-          <input type="tel" id="phone"/>
-          </div>
-            {/*Action buttons */}
-          <div className="d-flex">
-            <button className="btn text-success">Save</button>
-            <button className="btn text-danger" onClick={() => setEditprofile(false)}>Cancel</button>
-          </div>
-          </>
-        ) }
-          
+          {editProfile ? (
+            <>
+              <div className="mt-2">
+                {/*Name field*/}
+                <small>
+                  <label htmlFor="username">Name</label>
+                </small>
+                <input
+                  className="w-100"
+                  type="text"
+                  id="username"
+                  value={updatedDetails.username}
+                  onChange={(e) =>
+                    setUpdatedDetails((prevState) => ({
+                      ...prevState,
+                      username: e.target.value,
+                    }))
+                  }
+                />
+              </div>
+              {/*Address field*/}
+              <div className="">
+                <small>
+                  <label htmlFor="location">Address</label>
+                </small>
+                <input
+                  className="w-100"
+                  type="address"
+                  id="location"
+                  value={updatedDetails.address}
+                  onChange={(e) =>
+                    setUpdatedDetails((prevState) => ({
+                      ...prevState,
+                      address: e.target.value,
+                    }))
+                  }
+                />
+              </div>
+              {/*phone field*/}
+              <div className="">
+                <small>
+                  {" "}
+                  <label htmlFor="phone">Phone</label>
+                </small>
+                <input
+                  className="w-100"
+                  type="tel"
+                  id="phone"
+                  value={updatedDetails.phone}
+                  onChange={(e) =>
+                    setUpdatedDetails((prevState) => ({
+                      ...prevState,
+                      phone: e.target.value,
+                    }))
+                  }
+                />
+              </div>
+              {/*Action buttons */}
+              <div className="d-flex justify-content-between mt-2">
+                <button
+                  className="btn btn-success px-2 rounded-3 border-0"
+                  onClick={handleSaveProfile}
+                >
+                  Save
+                </button>
+                <button
+                  className="btn btn-danger px-2 rounded-3 border-0"
+                  onClick={() => setEditprofile(false)}
+                >
+                  Cancel
+                </button>
+              </div>
+            </>
+          ) : (
+            <button
+              className="btn btn-outline-primary w-100 p-0 mt-3"
+              onClick={handleEditProfile}
+            >
+              Edit Profile
+            </button>
+          )}
         </>
       )}
     </div>
